225d8004b21189a58bbf30e32b67ce6ca9870a08
hubertit <hubert@itdevs.rw>
2025-10-12 12:40:18 +0200

    Fix TimeOfDay import error in router
    - Add Flutter material import to app_router.dart
    - Resolves TimeOfDay type casting errors in router
    - Fixes 2 critical errors in flutter analyze
    - Router now properly handles TimeOfDay type from booking data

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
 import '../../features/auth/screens/login_screen.dart';
 import '../../features/auth/screens/register_screen.dart';
 import '../../features/auth/screens/onboarding_screen.dart';
