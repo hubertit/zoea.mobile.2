90a5bafe7f375669a81b68863427174340146320
hubertit <hubert@itdevs.rw>
2025-12-30 19:56:32 +0200

    feat: Add Irembo logo to Quick Actions
    - Downloaded Irembo logo SVG from https://irembo.gov.rw
    - Added flutter_svg package for SVG support
    - Integrated Irembo logo with color filter matching AppTheme.primaryColor
    - Logo uses same color as other Quick Actions icons
    - Replaced placeholder icon with actual Irembo logo

--- mobile/lib/features/explore/screens/explore_screen.dart
+++ mobile/lib/features/explore/screens/explore_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../core/theme/app_theme.dart';
 import '../../../core/widgets/fade_in_image.dart' show FadeInNetworkImage;
